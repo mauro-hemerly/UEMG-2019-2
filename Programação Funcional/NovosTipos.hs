874704f3c85967c3b7ae39d73ea35248f2f7af15
Mauro Hemerly Gazzani <mauro.hemerly@gmail.com>
2019-11-23 12:56:11 -0300

    Código Haskell para uso em sala de aula

--- Programação Funcional/NovosTipos.hs
+++ Programação Funcional/NovosTipos.hs
@@ -3,7 +3,81 @@
 -- Curso de Engenharia de Computação
 -- UEMG - Unidade de Ituiutaba
 
+
+-- Sinônimos de Tipos
+type Nome = String
+type SobreNome = String
+type Altura = Float
+type Peso = Int
+type PacienteT = (Nome, SobreNome,Altura,Peso)
+
+nomePaciente :: PacienteT -> Nome
+nomePaciente (nome,sobreN,altura,peso) = nome
+
+sobreNomePaciente :: PacienteT -> SobreNome
+sobreNomePaciente (nome,sobreN,altura,peso) = sobreN
+
+alturaPaciente :: PacienteT -> Altura
+alturaPaciente (nome,sobreN,altura,peso) = altura
+
+pesoPaciente :: PacienteT -> Peso
+pesoPaciente (nome,sobreN,altura,peso) = peso
+
+
 -- Definição de Novos Tipos de Dados
+
+data Sexo = Feminino | Masculino deriving Show
+
+abreviaturaSexo :: Sexo -> Char
+abreviaturaSexo Feminino = 'F'
+abreviaturaSexo Masculino = 'M'
+
+-- Tipagem sanguínea 
+data TipoRH = Positivo | Negativo deriving Show
+
+data TipoABO = A | B | AB | O deriving Show
+
+data TipoSangue = TipoSangue TipoABO TipoRH deriving Show
+
+showRh :: TipoRH -> String
+showRh Positivo = "+"
+showRh Negativo = "-"
+
+showABO :: TipoABO -> String
+showABO A = "A"
+showABO B = "B"
+showABO AB = "AB"
+showABO O = "O"
+
+showTipoSanguineo :: TipoSangue -> String
+showTipoSanguineo (TipoSangue abo rh) = showABO abo ++ showRh rh
+
+
+quemPodeDoar :: TipoSangue -> TipoSangue -> Bool
+quemPodeDoar (TipoSangue O _) _ = True    -- Doador universal
+quemPodeDoar _ (TipoSangue AB _) = True   -- Receptor universal
+quemPodeDoar (TipoSangue A _) (TipoSangue A _) = True
+quemPodeDoar (TipoSangue B _) (TipoSangue B _) = True
+quemPodeDoar _ _ = False  
+
+paciente1 = TipoSangue A Positivo
+paciente2 = TipoSangue O Negativo
+paciente3 = TipoSangue AB Positivo
+
+data Paciente = Paciente Nome SobreNome Altura Peso Idade Sexo TipoSangue deriving Show
+ 
+
+getNome :: Paciente -> Nome
+getNome (Paciente nome _ _ _ _ _ _) = nome
+
+getIdade :: Paciente -> Idade
+getIdade (Paciente _ _ _ _ idade _ _) = idade
+
+getSexo :: Paciente -> Sexo
+getSexo (Paciente _ _ _ _ _ sexo _) = sexo
+
+getTipoSangue :: Paciente -> TipoSangue
+getTipoSangue (Paciente _ _ _ _ _ _ sangue) = sangue
 
 data Cor = Vermelho | Verde | Azul deriving Eq
 
@@ -11,6 +85,15 @@ data Mes = Jan | Fev | Mar | Abr | Mai | Jun | Jul | Ago | Set | Out | Nov | Dez
 
 data Endereco = Rua String Int Complemento
 data Complemento = Casa | Apartamento
+
+type Idade = Int
+type Salario = Float
+
+data Funcionario = Executivo Nome Idade Salario | 
+                   VicePresidente String Idade Salario |
+                   Gerente Nome Idade Salario |
+                   Engenheiro Nome Idade Salario
+
 
 -- Árvore Binária
 data ArvoreBinaria a = No (ArvoreBinaria a) a (ArvoreBinaria a) | Null
@@ -112,11 +195,11 @@ instance (Compara a, Compara b, Compara c) => Compara (a,b,c) where
 
 -------------------------------------
 
-iguala :: NomeCompleto -> NomeCompleto -> Bool
-iguala (Nome x) (Nome y) = x == y
+--iguala :: NomeCompleto -> NomeCompleto -> Bool
+--iguala (Nome x) (Nome y) = x == y
 
 
-data NomeCompleto = Nome String deriving Eq
+--data NomeCompleto = Nome String deriving Eq
 --instance Eq NomeCompleto where
 --  (==) = iguala 
 
